4f5893424329aa4332d6b9a9f8de10e9f15d5450
harikasayani <143275265+harikasayani@users.noreply.github.com>
2023-10-20 06:29:48 +0530

    Add files via upload

---
+++ project3.js
@@ -0,0 +1,18 @@
+function myfunc(valnum){
+    if(valnum==1){
+   document.getElementById("demo").innerHTML="Netflix is a streaming service  that offers wide varity of TV shows,movies";
+    }
+    else if(valnum==2){
+   document.getElementById("demo1").innerHTML="Plans range from Rs.149 to Rs.649 a month";
+    }
+    else if(valnum==3){
+        document.getElementById("demo2").innerHTML="Watch anywhere anytime .You can also downlad your favourite shows";
+    }
+    else if(valnum==4){
+        document.getElementById("demo3").innerHTML="Netflix is flexible.There are no annoying contracts and no commitmentsl";
+    }
+    else if(valnum==5){
+        document.getElementById("demo4").innerHTML="Netflix has a extensive library of feature films,documentaries,TV shows and more";
+    }
+   
+}
